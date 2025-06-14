22a894f82861ad60b67b4d3923ea47e65fee32d7
nashdev255 <nashdev255@gmail.com>
2025-06-14 19:26:00 +0900

    change: base defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'http://astronaut.github.io',
-  base: 'nash-portfolio-2025',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
